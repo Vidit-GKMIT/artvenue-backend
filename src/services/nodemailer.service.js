944728410632a6a848af8cfdb57631357fc97944
Vidit-GKMIT <vidit@gkmit.co>
2025-11-14 11:02:07 +0530

    feat: add verify email availability controller and add otp verification controller for register route

--- src/services/nodemailer.service.js
+++ src/services/nodemailer.service.js
@@ -1,46 +1,32 @@
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer'
+import ejs from 'ejs'
+import path from 'path'
 
-// const transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: 'khandelwal7vidit@gmail.com',
-//     pass: 'dozs zwmy hczr xmhn', // App password from Google
-//   },
-// });
-
-// export const sendOtpEmail = async (recipientEmail,expirationTime, otp, userName) => {
-//   const info = await transporter.sendMail({
-//     from: '"Art Venue" <khandelwal7vidit@gmail.com>',
-//     to: recipientEmail,
-//     subject: "OTP For Signin in ArtVenue",
-//     text: `Hello ${userName},\n\nYour OTP for signing in to ArtVenue is: ${otp}\nThis OTP is valid for ${expirationTime} minutes.\n\nIf you did not request this, please ignore this email.\n\nBest regards,\nArtVenue Team`,
-//     html: `Hello ${userName},\n\nYour OTP for signing in to ArtVenue is: ${otp}\nThis OTP is valid for ${expirationTime} minutes.\n\nIf you did not request this, please ignore this email.\n\nBest regards,\nArtVenue Team`,
-//   });
-//   console.log("Message sent:", info.messageId);
-//   return info;
-// }
-
-
-
-export const sendOtpEmail = async (recipientEmail,expirationTime, otp, userName) => {
-  const testAccount = await nodemailer.createTestAccount();
+export const sendOtpEmail = async (
+  recipientEmail,
+  expirationTime,
+  otp,
+  userName
+) => {
+  const html = await ejs.renderFile(
+    path.join(__dirname, 'templates', 'email.template.ejs'),
+    { otp, userName, expiryMinutes: expirationTime }
+  )
   const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
+    service: 'gmail',
     auth: {
-        user: 'mireya.runte@ethereal.email',
-        pass: 'VNyZ1cT3rkCkhPmcx2'
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD // App password from Google
     }
-});
+  })
 
   let info = await transporter.sendMail({
-    from: '"ArtVenue " <artvenue@gmail.com>',
+    from: '"ArtVenue" <artvenue@gmail.com>',
     to: recipientEmail,
-    subject: "Hello ✔",
-    text: "Hello world?", // plain‑text body
-    html: "<b>Hello world?</b>", // HTML body
-    })
-    console.log(info);
-  console.log("Message sent:", info.messageId);
-  return info;
+    subject: 'OTP for ArtVenue',
+    text: `Your verification code is: ${otp}. This code will expire in 5 minutes.`,
+    html: html
+  })
+  console.log('Message sent:', info.messageId)
+  return info
 }
