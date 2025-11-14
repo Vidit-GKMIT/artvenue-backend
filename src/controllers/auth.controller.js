0bc1f1b421e9f401cc96bba0728bdf1f0ad830b8
Vidit-GKMIT <vidit@gkmit.co>
2025-11-14 11:02:07 +0530

    feat: add verify email availability controller and add otp verification controller for register route

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,30 +1,5 @@
 import { prisma } from '../db/postgres.db.js'
 
-export const verifyEmail = async (req, res) => {
-    try {
-        const email = req.body.email
-        const existedEmail = await prisma.users.findUnique({
-            where: { email },
-        })
-
-        if (existedEmail) {
-            return res
-                .status(400)
-                .json({ message: 'Email already exists', success: false })
-        }
-
-        return res
-            .status(200)
-            .json({ message: 'Email is available', success: true })
-    } catch (error) {
-        return res.status(500).json({
-            message: 'Internal server error',
-            success: false,
-            error: error.message,
-        })
-    }
-}
-
 export const register = (req,res) => {
     return res.status(200).json({ message: "register route" });
 }
