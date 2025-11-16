aae069c922c4407fe9edff00c6934b9f82fb0c57
Vidit-GKMIT <vidit@gkmit.co>
2025-11-16 17:11:21 +0530

    refactor: remove unecessary logs

--- src/middlewares/verifyRole.middleware.js
+++ src/middlewares/verifyRole.middleware.js
@@ -1,16 +1,20 @@
 export const authorizeRole = (role) => {
   return (req, res, next) => {
-     if(!req.user){
-      return res.status(401).json({ message: "Unauthorized. Please login again.", success: false });
-     }
+    if (!req.user) {
+      return res
+        .status(401)
+        .json({ message: 'Unauthorized. Please login again.', success: false })
+    }
 
-     console.log(req.user)
-     console.log(role)
-
-     if(req.user.role !== role){
-      return res.status(403).json({ message: "Sorry you are not authorised to access this resource.", success: false });
-     }
-     console.log(req.user.role);
-     next();
+    if (req.user.role !== role) {
+      return res
+        .status(403)
+        .json({
+          message: 'Sorry you are not authorised to access this resource.',
+          success: false
+        })
+    }
+    console.log(req.user.role)
+    next()
   }
 }
