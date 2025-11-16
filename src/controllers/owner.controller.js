1f9b3bb7421657b2cedb27a47e2e2a086e521678
Vidit-GKMIT <vidit@gkmit.co>
2025-11-16 11:10:05 +0530

    feat: create event functionality for owner

--- src/controllers/owner.controller.js
+++ src/controllers/owner.controller.js
@@ -1,4 +1,4 @@
-import { findAllArtists, createVenueInDB, updateVenueInDB } from '../services/owner.service.js'
+import { findAllArtists, createVenueInDB, updateVenueInDB, createEventInDB } from '../services/owner.service.js'
 
 export const getAllArtists = async (req, res) => {
   try {
@@ -48,7 +48,7 @@ export const updatVenue = async (req,res) => {
     const updateVenueData = await updateVenueInDB(req.params.venueId, data, req.user.id);
 
     if (!updateVenueData) {
-      return res.status(401).json({
+      return res.status(403).json({
         message: 'You are not authorized to update this venue.',
         success: false
       });
@@ -63,4 +63,22 @@ export const updatVenue = async (req,res) => {
   } catch (error) {
     res.status(500).json({ message: 'Internal server error.', success: false,  error: error.message })
   }
+}
+
+export const createEvent = async (req, res) => {
+  const data = req.validatedData;
+  const createdEvent = await createEventInDB(data, req.user.id);
+
+  if(createdEvent === 'Venue not found for the owner.') {
+    return res.status(404).json({
+      message: 'Venue not found for the owner. Please create a venue first.',
+      success: false
+    });
+  }
+
+  return res.status(201).json({
+    message: 'Event created successfully.',
+    data: createdEvent,
+    success: true
+  });
 }
--- src/routes/owner.route.js
+++ src/routes/owner.route.js
@@ -1,13 +1,16 @@
 import express from 'express'
-import { getAllArtists, createVenue } from '../controllers/owner.controller.js'
-import { validateVenue } from '../validators/owner.validator.js';
+import { getAllArtists, createVenue, updatVenue, createEvent } from '../controllers/owner.controller.js'
+import { validateVenue, validateEvent } from '../validators/owner.validator.js';
+import { authorizeRole } from '../middlewares/verifyRole.middleware.js'
 
 
 const router = express.Router()
+router.use(authorizeRole('Owner'));
 
 router
   .get('/allArtists', getAllArtists)
   .post("/createVenue", validateVenue, createVenue)
-
+  .patch("/updateVenue/:venueId", validateVenue, updatVenue)
+  .post("/createEvent", validateEvent, createEvent)
 
 export default router
