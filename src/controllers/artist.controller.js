36586ea36c6b45c1ef0ebbdec78b677594fa27df
Vidit-GKMIT <vidit@gkmit.co>
2025-11-20 16:44:46 +0530

    feat: This PR introduces all features that enable artists to interact with events and venues. (#4)
    Last pr for my backend, and done with all functionalities. Just remaining with some small issues.

---
+++ src/controllers/artist.controller.js
@@ -0,0 +1,90 @@
+import {
+  findAllVenuesFromDB,
+  findAllEventsFromDB,
+  sendEmail, 
+  createOptInEntry,
+} from '../services/artist.service.js'
+
+export const getAllVenues = async (req, res) => {
+  try {
+    const allEvents = await findAllVenuesFromDB()
+    if (!allEvents) {
+      return res.status(200).json({
+        message: 'No venues found.',
+        success: true,
+        data : []
+      })
+    }
+    return res.status(200).json({
+      message: 'All venues fetched successfully',
+      data: allEvents,
+      success: true
+    })
+  } catch (error) {
+    return res.json({
+      message: 'Internal server error',
+      success: false,
+      error: error.message
+    })
+  }
+}
+
+export const getAllEvents = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const allEvents = await findAllEventsFromDB(userId)
+    if (!allEvents) {
+      return res.status(404).json({
+        message: 'No events found',
+        success: false
+      })
+    }
+
+    return res.status(200).json({
+      message: 'All events fetched successfully',
+      data: allEvents,
+      success: true
+    })
+  } catch (error) {
+    return res.json({
+      message: 'Internal server error',
+      success: false,
+      error: error.message
+    })
+  }
+}
+
+export const optInEvent = async (req, res) => {
+  try {
+    const artistId = req.user.id
+    const { eventId } = req.params
+    if (!eventId) {
+      return res.status(400).json({
+        message: 'Event ID is required',
+        success: false
+      })
+    }
+
+    const optInEntry = createOptInEntry(artistId, eventId);
+
+     if(!optInEntry){
+      return res.status(500).json({
+        message: 'Failed to opt-in please try again later.',
+        success: false
+      })
+     }
+
+    const emailSent = sendEmail(artistId, eventId)
+
+    return res.status(200).json({
+        success: true,
+        message: "Email have been sent to venue owner. He will connect to you soon."
+    })
+  } catch (error) {
+    return res.json({
+      message: 'Internal server error',
+      success: false,
+      error: error.message
+    })
+  }
+}
--- src/routes/venue.route.js
+++ src/routes/venue.route.js
@@ -1,17 +1,22 @@
 import express from 'express'
 import {
   createVenue,
-  updatVenue,
+  updateVenue,
 } from '../controllers/owner.controller.js'
 import { validateVenue } from '../validators/owner.validator.js'
 import { authorizeRole } from '../middlewares/verifyRole.middleware.js'
+import { getAllVenues } from '../controllers/artist.controller.js'
+import { getAllVenuesForParticularOwner } from '../controllers/owner.controller.js'
 
 const router = express.Router()
-router.use(authorizeRole('Owner'))
 
-router.post('/', validateVenue, createVenue)
-      .patch('/:venueId', validateVenue, updatVenue)
+//for owners
+router.post('/',[authorizeRole('Owner'), validateVenue],  createVenue)  //create venue
+      .patch('/:venueId',[authorizeRole('Owner'), validateVenue], updateVenue) //update venue 
+      .get('/:ownerId',authorizeRole('Owner'), getAllVenuesForParticularOwner) //get all venues of an owner
+    
+//for artists
+router.get('/',authorizeRole('Artist'), getAllVenues)
 
 
 export default router
-
